82212ea5be704df0a079128dd5759c079e531fa8
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-04-17 14:07:42 +0800

    Create WorkerManger.h

---
+++ Staff Management/WorkerManger.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+class WorkerMager
+{
+public:
+	//构造函数
+	WorkerMager();
+
+	//菜单展示函数
+	void ShowMenu();
+
+
+
+	~WorkerMager();
+};
