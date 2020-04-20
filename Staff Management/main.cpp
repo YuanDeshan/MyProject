14d58a46323f82dd50455c726451b3bd9825de18
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-04-20 20:35:18 +0800

    Update main.cpp

--- Staff Management/main.cpp
+++ Staff Management/main.cpp
@@ -2,7 +2,37 @@
 
 int main()
 {
-	WorkerMager wm;
+	WorkerManger wm;
+	int choice=0;
+
+	while (true)
+	{
 	wm.ShowMenu();
+	cin >> choice;
+	switch (choice)
+	{
+	case 0:
+		wm.ExitSystem();
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	default:
+		system("cls");
+		break;
+	}
+
+	}
 	return 0;
 }
